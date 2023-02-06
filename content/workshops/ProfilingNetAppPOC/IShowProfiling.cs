caa80c365be1a9c310d56405be72e2061ba20e61
Edelberto Reyes <ereyest@rentingcolombia.com>
2023-02-06 14:09:30 -0500

    Profiling Net apps examples.

---
+++ content/workshops/ProfilingNetAppPOC/IShowProfiling.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfilingNetAppPOC
+{
+    public interface IShowProfiling
+    {
+        void showProfiling();
+    }
+}
---
+++ content/workshops/ProfilingNetAppPOC/ObjectAllocation/Flyweight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfilingNetAppPOC.ObjectAllocation
+{
+    public abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+}
